603c1360ad60207c0f4909d3aa6e4e8950420e60
Alexandre <alextsndev@gmail.com>
2024-05-03 16:17:37 +0200

    Mise a jour format montant et ajout des commentaires

--- convertirdevise.py
+++ convertirdevise.py
@@ -1,24 +1,31 @@
 # Creer une application pour convertir un montant en devise dans une autre devise. 
 # Le montant est a saisir, la devise de départ est a saisir, ainsi que la devise d'arrivée
+
 import requests
 
-deviseInitiale= input("Ecrivez le code de la devise initiale   ") # demande de la devise initiale
-montant= float(input('Saisissez un montant à convertir   ')) # demande d'une valeur à convertir
-deviseFinale=input("Ecrivez le code de la devise finale   ") # demande de la devise finale
+# demande de la devise initiale
+deviseInitiale= input("Ecrivez le code de la devise initiale   ") 
+# demande d'une valeur à convertir
+montant= float(input('Saisissez un montant à convertir   ')) 
+ # demande de la devise finale
+deviseFinale=input("Ecrivez le code de la devise finale   ")
+
+montant_format="{:,.2f}".format(float(montant)).replace(","," ")
 
 #creation d'une variable URL pour renvoyer vers l'API de la valeur finale
 url="https://open.er-api.com/v6/latest/" + deviseInitiale
-print(url)
-
-response = requests.get(url) # requete vers l'API 
+ # requete vers l'API 
+response = requests.get(url)
 
 if response.status_code ==200: 
-    data= response.json() #convertir la réponse en format JSON
-    # print(data) #afficher les données JSON
-    valeurDeviseFinale=float(data['rates'][deviseFinale]) #Extrait la valeur de la conversion entre Devise Initiale et Devise Finale
-    # print(valeurDeviseFinale)
-    convertir = round(float(montant * valeurDeviseFinale),2) #convertis la valeur , 2 décimales
-    print("La valeur de " + str(montant) +" "+ (deviseInitiale) +" en "+ (deviseFinale) + " est de "+ str(convertir)+ ' ' + (deviseFinale)) # print du résultat
+    #convertir la réponse en format JSON
+    data= response.json() 
+    #Extrait la valeur de la conversion entre Devise Initiale et Devise Finale
+    valeurDeviseFinale=float(data['rates'][deviseFinale]) 
+    
+    convertir="{:,.2f}".format(float(montant * valeurDeviseFinale)).replace(","," ")
+    # print du résultat
+    print("La valeur de " + str(montant_format) +" "+ (deviseInitiale) +" en "+ (deviseFinale) + " est de "+ str(convertir)+ ' ' + (deviseFinale)) 
 else : 
     print("La requete à échouer avec le code :", response.status_code)
     
